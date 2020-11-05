c048ecf2cdbd5e7ef13494cad59c5ea4e6c4251e
Kais Temimi <kassus007@gmail.com>
2020-11-04 17:34:48 -0800

    python

--- script.py
+++ script.py
@@ -25,5 +25,13 @@
 
 #########
 # INSERT YOUR CODE BELOW THIS LINE
+l = []
+for i in range(2000, 3201):
+    if (i % 7 == 0) and (i % 5 !=0):
+        l.append(str(i))
+
+print ','.join(l)
 #########
+
+
 
